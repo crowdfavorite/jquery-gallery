cf4d2f2a0b925a9382598a2c1bf1bc647a32a035
aking <aking@546ca710-db30-0410-8887-d9dd8e66aff0>
2011-12-22 08:20:00 +0000

    show caption on initial load, then fade out
    git-svn-id: https://svn.crowdfave.org/svn/crowdfavorite/code/jquery-plugins/cfgallery/trunk@42010 546ca710-db30-0410-8887-d9dd8e66aff0

--- jquery.cfgallery.js
+++ jquery.cfgallery.js
@@ -11,7 +11,7 @@
  * Dual licensed under the MIT and GPL licenses.
  * http://benalman.com/about/license/
  */
-(function($,e,b){var c="hashchange",h=document,f,g=$.event.special,i=h.documentMode,d="on"+c in e&&(i===b||i>7);function a(j){j=j||location.href;return"#"+j.replace(/^[^#]*#?(.*)$/,"$1")}$.fn[c]=function(j){return j?this.bind(c,j):this.trigger(c)};$.fn[c].delay=50;g[c]=$.extend(g[c],{setup:function(){if(d){return false}$(f.start)},teardown:function(){if(d){return false}$(f.stop)}});f=(function(){var j={},p,m=a(),k=function(q){return q},l=k,o=k;j.start=function(){p||n()};j.stop=function(){p&&clearTimeout(p);p=b};function n(){var r=a(),q=o(m);if(r!==m){l(m=r,q);$(e).trigger(c)}else{if(q!==m){location.href=location.href.replace(/#.*/,"")+q}}p=setTimeout(n,$.fn[c].delay)}$.browser.msie&&!d&&(function(){var q,r;j.start=function(){if(!q){r=$.fn[c].src;r=r&&r+a();q=$('<iframe tabindex="-1" title="empty"/>').hide().one("load",function(){r||l(a());n()}).attr("src",r||"javascript:0").insertAfter("body")[0].contentWindow;h.onpropertychange=function(){try{if(event.propertyName==="title"){q.document.title=h.title}}catch(s){}}}};j.stop=k;o=function(){return a(q.location.href)};l=function(v,s){var u=q.document,t=$.fn[c].domain;if(v!==s){u.title=h.title;u.open();t&&u.write('<script>document.domain="'+t+'"<\/script>');u.close();q.location.hash=v}}})();return j})()})(jQuery,this);
+;(function($,e,b){var c="hashchange",h=document,f,g=$.event.special,i=h.documentMode,d="on"+c in e&&(i===b||i>7);function a(j){j=j||location.href;return"#"+j.replace(/^[^#]*#?(.*)$/,"$1")}$.fn[c]=function(j){return j?this.bind(c,j):this.trigger(c)};$.fn[c].delay=50;g[c]=$.extend(g[c],{setup:function(){if(d){return false}$(f.start)},teardown:function(){if(d){return false}$(f.stop)}});f=(function(){var j={},p,m=a(),k=function(q){return q},l=k,o=k;j.start=function(){p||n()};j.stop=function(){p&&clearTimeout(p);p=b};function n(){var r=a(),q=o(m);if(r!==m){l(m=r,q);$(e).trigger(c)}else{if(q!==m){location.href=location.href.replace(/#.*/,"")+q}}p=setTimeout(n,$.fn[c].delay)}$.browser.msie&&!d&&(function(){var q,r;j.start=function(){if(!q){r=$.fn[c].src;r=r&&r+a();q=$('<iframe tabindex="-1" title="empty"/>').hide().one("load",function(){r||l(a());n()}).attr("src",r||"javascript:0").insertAfter("body")[0].contentWindow;h.onpropertychange=function(){try{if(event.propertyName==="title"){q.document.title=h.title}}catch(s){}}}};j.stop=k;o=function(){return a(q.location.href)};l=function(v,s){var u=q.document,t=$.fn[c].domain;if(v!==s){u.title=h.title;u.open();t&&u.write('<script>document.domain="'+t+'"<\/script>');u.close();q.location.hash=v}}})();return j})()})(jQuery,this);
 
 ;(function ($, win) {
 	/* Local variable for hash makes lookups faster and is better for closure compiler */
@@ -156,7 +156,7 @@
 				
 				this.$thumbs.removeClass(c);
 				$thumb.addClass(c);
-
+				
 				this.preloadNeighbors(i);
 				this.current = i;
 			};
@@ -175,14 +175,27 @@
 		
 		/* Allow transition to be overidden using Duck Punching */
 		transitionSlides: function ($neue, $old) {
+			var _this = this;
 			if ($old !== null && typeof $old !== 'undefined') {
-				$old.fadeOut('fast', function(){
-					$neue.fadeIn('medium');
+				$old.stop().removeClass('init').fadeOut('fast', function() {
+					_this.showSlide($neue);
 				});
 			}
 			else {
-				$neue.fadeIn('medium');
+				_this.showSlide($neue);
 			};
+			// show captions
+		},
+		
+		showSlide: function ($neue) {
+// TODO - still getting some doubling of caption display on rapid keypress
+			$neue.stop().addClass('init').fadeIn('medium', function() {
+				$(this).animate({
+					opacity: 1
+				}, 1600, function() {
+					$(this).removeClass('init');
+				});
+			});
 		},
 		
 		/* Get ID token from hash string */
