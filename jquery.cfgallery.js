c117162e672361b3292bf989f79944ca9c1021f3
Alex King <alex@alexking.org>
2012-12-11 16:59:29 -0700

    updating version to 1.1

--- jquery.cfgallery.js
+++ jquery.cfgallery.js
@@ -1,6 +1,6 @@
 /*!
  * cfgallery - a light-weight, semantic gallery script with bookmarkable slides.
- * version 1.0
+ * version 1.1
  *
  * Copyright (c) 2011-2012 Crowd Favorite (http://crowdfavorite.com)
  */
